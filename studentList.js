26d9789db853c6455f16da733040b90f72d8bc53
Gil <gil.borenstein@gmail.com>
2020-05-05 11:47:23 -0400

    enhanced time update code

--- studentList.js
+++ studentList.js
@@ -1,19 +1,28 @@
+var priorTime = "00:01";
+
 $(document).ready(function(){
     //define interval function
     document.getElementById("timeInput").value = "00:01";
     updateTime();
-   setInterval(function(){updateTime()}, 15000);
+    setInterval(function(){updateTime()},10000);
 });
-
-
 
 function updateTime(){
     var d = new Date();
-    var h = d.getHours();
-    var m = d.getMinutes();
-    console.log("updating time. " + h + ":" + m)
+    var h = formatTime(d.getHours());
+    var m = formatTime(d.getMinutes());
     var s = h + ":" +m;
-    //$("#timeInput").value(h + ":" + m);
-    //TODO: change this to jquery. maybe.
-    document.getElementById("timeInput").value = s; 
+    var $time = $("#timeInput");
+    var v = $time.val();
+    if($time.val() != s){
+        console.log("updating time. " + h + ":" + m);
+        $time.val(s);
+    }
+}
+
+function formatTime(x){
+    if(x<10){
+        x = "0" + x;
+    }
+    return x;
 }
