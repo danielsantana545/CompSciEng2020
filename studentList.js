62d5492332f4f7261ef9aab771b777d44b2a6fe7
Gil <gil.borenstein@gmail.com>
2020-05-15 18:33:45 -0400

    CSS for arbetrarly sized table

--- studentList.js
+++ studentList.js
@@ -52,8 +52,9 @@ function getCurrentTime(){ //returns current 24 hour time as hh:mm
 
 function reloadTable(){
 
-    addToTable(createStudentObject("adom","05:21","database"))
-    addToTable(createStudentObject("beetrice","05:21","database"))
+    addToTable(createStudentObject("adom","05:21","database"));
+    addToTable(createStudentObject("beetrice","05:21","database"));
+    addToTable(createStudentObject("pissboi","05:21","database"))
 
     function addToTable(studentEvent){
         var tableRow = "<tr><td class='studentRow'><p>" + studentEvent.name + "</p>" + notes + timeOut + editButton + submitButton + "</td></tr>";
