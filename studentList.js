f1a0ec3f64b2af0e650e66682202051acb60370b
danielsantana545 <55592739+danielsantana545@users.noreply.github.com>
2020-05-17 13:29:29 -0700

    pulled changes from develop branch

--- studentList.js
+++ studentList.js
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-const notes = '<input type="text" class="notes">';
-const timeOut = '<input type="time" class="timeOut">';
-const submitButton = '<input type="image" src="https://img.icons8.com/ultraviolet/40/000000/edit.png" class="editButton"/>';
-const editButton = '<input type="image" src="https://img.icons8.com/ultraviolet/40/000000/checked-2.png" class="submitButton"/>';
-
-
-var priorTime = null;//used for allowing the user to edit the time
-var studentArray = [];
-
-$(document).ready(function () {
-    reloadTable();
-    //define interval function
-    document.getElementById("timeInput").value = "00:01";
-    updateTime();
-    setInterval(function () { updateTime() }, 10000);
-});
-
-function timeEdit() {//called when user manual changes time
-    priorTime = getCurrentTime();
-}
-
-function updateTime() { //updates the time on the checkin time
-    var s = getCurrentTime();
-    var $time = $("#timeInput");
-    var v = $time.val();
-    if (priorTime !== null) { //this block keeps the update from undoing manual edits
-        if (priorTime != s) {
-            console.log("updating time. " + s);
-            $time.val(s);
-            priorTime = null; //setting priorTime to null
-        }
-    }
-    else if ($time.val() != s) {
-        console.log("updating time. " + s);
-        $time.val(s);
-=======
 const notes = '<textarea placeholder="notes" class="notes"></textarea>';
 const timeOut = '<input type="time" class="timeOut">';
 const submitButton = '<input type="image" src="https://img.icons8.com/ultraviolet/40/000000/edit.png" class="editButton"/>';
@@ -57,21 +20,13 @@ function reloadTable(){
 
     function clearTable(){
         $(".studentTable").find("tr").remove();
->>>>>>> 0aefb979a951fdd8110fd2b8b4b29b17ea46697c
     }
 
-<<<<<<< HEAD
-function getCurrentTime() { //returns current 24 hour time as hh:mm
-    function formatTime(x) { //makes sure all times are 2 digit stings
-        if (x < 10) {
-            x = "0" + x;
-=======
     function addToTable(item){
         var tableRow = "<tr><td class='studentRow'><p>" + item.name + "</p>" + notes + timeOut + editButton + submitButton + "</td></tr>";
         var trTst = $(".studentTable tr:last");
         if($(".studentTable tr:last").length !== 0){
             $(".studentTable tr:last").after(tableRow);
->>>>>>> 0aefb979a951fdd8110fd2b8b4b29b17ea46697c
         }
         else {
             $(".studentTable").append(tableRow);
@@ -90,12 +45,6 @@ function createStudentObject(name, timeIn, course){
     };
 }
 
-<<<<<<< HEAD
-    var d = new Date();
-    var h = formatTime(d.getHours());
-    var m = formatTime(d.getMinutes());
-    return h + ":" + m;
-}
 
 function reloadTable() {
     clearTable();
@@ -127,22 +76,3 @@ function createStudentObject(name, timeIn, course) {
         notes: null
     };
 }
-=======
-function sendToDatabase(studentEvent){
-    $.post("insertInfo.php",
-        {
-            student_Name: studentEvent.name,
-            time_In : studentEvent.timeIn,
-            class_name : studentEvent.course,
-            time_Out : studentEvent.timeOut,
-            notes : studentEvent.notes
-        },
-        function(data) {
-           console.log(data
-            )
-        }
-     );
-}
-
-
->>>>>>> 0aefb979a951fdd8110fd2b8b4b29b17ea46697c
