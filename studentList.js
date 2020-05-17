0aefb979a951fdd8110fd2b8b4b29b17ea46697c
Gil <gil.borenstein@gmail.com>
2020-05-17 15:13:34 -0400

    Seperated time updating to TimeUpdater.js

--- studentList.js
+++ studentList.js
@@ -3,8 +3,6 @@ const timeOut = '<input type="time" class="timeOut">';
 const submitButton = '<input type="image" src="https://img.icons8.com/ultraviolet/40/000000/edit.png" class="editButton"/>';
 const editButton = '<input type="image" src="https://img.icons8.com/ultraviolet/40/000000/checked-2.png" class="submitButton"/>';
 
-
-var priorTime = null;//used for allowing the user to edit the time
 var studentArray = [];
 
 $(document).ready(function(){
@@ -12,47 +10,9 @@ $(document).ready(function(){
     studentArray.push(createStudentObject("alpha nerd","07:33","database"));
     studentArray.push(createStudentObject("rojer","07:33","database"));
     reloadTable();
-
-    //Ready time functions + interval
-    document.getElementById("timeInput").value = "00:01";
-    updateTime();
-    setInterval(function(){updateTime()},10000);
 });
 
-function timeEdit(){//called when user manual changes time
-    priorTime = getCurrentTime();
-}
 
-function updateTime(){ //updates the time on the checkin time
-    var s = getCurrentTime();
-    var $time = $("#timeInput");
-    var v = $time.val();
-    if(priorTime !== null){ //this block keeps the update from undoing manual edits
-        if(priorTime != s){
-            console.log("updating time. " + s);
-            $time.val(s);
-            priorTime = null; //setting priorTime to null
-        }
-    }
-    else if($time.val() != s){
-        console.log("updating time. " + s);
-        $time.val(s);
-    }
-}
-
-function getCurrentTime(){ //returns current 24 hour time as hh:mm
-    function formatTime(x){ //makes sure all times are 2 digit stings
-        if(x<10){
-            x = "0" + x;
-        }
-        return x;
-    }
-
-    var d = new Date();
-    var h = formatTime(d.getHours());
-    var m = formatTime(d.getMinutes());
-    return h + ":" +m;
-}
 
 function reloadTable(){
     clearTable();
